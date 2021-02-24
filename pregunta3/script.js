819daa305a48b52803830be702084646d1d17256
TeresaEngelhard <mat.engelhard@gmail.com>
2021-02-23 21:05:46 -0600

    parcial finalizado

---
+++ pregunta3/script.js
@@ -0,0 +1,27 @@
+function sendForm() {
+    var partido = document.getElementById("partido").value
+    var orientacion = document.getElementById("orientacion").value
+    var votos = document.getElementById("votos").value
+
+    if (votos>50){
+        var estado = ("+"+votos);  
+    } else if (votos<50){
+        var estado = ("-"+votos);
+    } else if (votos=50){
+        var estado = ("="+votos);
+    }
+
+    document.getElementById("tabla").insertRow(-1).innerHTML = '<tr><td>'+partido+'</td><td>'+orientacion+'</td><td>'+votos+'</td><td>'+estado+'</td></tr>';
+}
+
+function sendWinner(){
+
+    // var table = document.getElementById("tabla")
+    // var rows = table.rows[i].getElementsByTagName('td')
+
+    // for (var i = 1; i < table.rows.length; i++){
+    //     if table.rows[i]
+    // }
+
+
+}
